8fc5e7a4524c2432fd790ddb1ca571769d80b70b
Dmitry <df.creative@gmail.com>
2014-11-23 16:38:17 +0300

    Initial

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# color-convert
+# color-space
+
+Math and data behind the color spaces and conversions. The most complete color-conversions node module so far. Initially fork of [color-convert](https://github.com/dfcreative/color), separated to a standalone module.
+
+Differences with initial color-convert:
+
+* no keywords (isn’t a space - actually, it is a different king of knowledge + extra bytes saved)
+* no wrapper code (you have a natural wrapper - [color.js](https://github.com/dfcreative/color))
+	* conversions.js are placed to index.js for better use
+* CIE spaces
+* HUSL spaces (thanks to Alexei Boronine)
+* Ranges list
+* Better structured (no need `2` in function name)
+* No rounding result - one can always do it himself
+
 Color-convert is a color conversion library for JavaScript and node. It converts all ways between `rgb`, `hsl`, `hsv`, `hwb`, `cmyk`, and CSS keywords:
 
 ```javascript
@@ -7,7 +21,7 @@ var converter = require("color-convert")();
 converter.rgb(140, 200, 100).hsl()   // [96, 48, 59]
 
 converter.keyword("blue").rgb()      // [0, 0, 255]
-```	
+```
 
 # Install
 
@@ -15,25 +29,13 @@ converter.keyword("blue").rgb()      // [0, 0, 255]
 
 For [node](http://nodejs.org) with [npm](http://npmjs.org):
 
-	npm install color-convert
+	npm install dfcreative/color-convert
 
 ### browser
 
 Download the latest [color-convert.js](http://github.com/harthur/color-convert/downloads). All the methods are on the `colorConvert` object.
 
 # API
-Color-convert exports a converter object with getter/setter methods for each color space. It caches conversions:
-
-```javascript
-var converter = require("color-convert")();
-
-converter.rgb(140, 200, 100).hsl()   // [96, 48, 59]
-
-converter.rgb([140, 200, 100])       // args can be an array
-```
-
-### Plain functions
-Get direct conversion functions with no fancy objects:
 
 ```javascript
 require("color-convert").rgb2hsl([140, 200, 100]);   // [96, 48, 59]
--- index.js
+++ index.js
@@ -1,92 +1,502 @@
-var conversions = require("./conversions");
+var rgb = {
+  hsl: function(rgb) {
+    var r = rgb[0]/255,
+        g = rgb[1]/255,
+        b = rgb[2]/255,
+        min = Math.min(r, g, b),
+        max = Math.max(r, g, b),
+        delta = max - min,
+        h, s, l;
 
-var convert = function() {
-   return new Converter();
-}
+    if (max == min)
+      h = 0;
+    else if (r == max)
+      h = (g - b) / delta;
+    else if (g == max)
+      h = 2 + (b - r) / delta;
+    else if (b == max)
+      h = 4 + (r - g)/ delta;
 
-for (var func in conversions) {
-  // export Raw versions
-  convert[func + "Raw"] =  (function(func) {
-    // accept array or plain args
-    return function(arg) {
-      if (typeof arg == "number")
-        arg = Array.prototype.slice.call(arguments);
-      return conversions[func](arg);
+    h = Math.min(h * 60, 360);
+
+    if (h < 0)
+      h += 360;
+
+    l = (min + max) / 2;
+
+    if (max == min)
+      s = 0;
+    else if (l <= 0.5)
+      s = delta / (max + min);
+    else
+      s = delta / (2 - max - min);
+
+    return [h, s * 100, l * 100];
+  },
+
+  hsv: function(rgb) {
+    var r = rgb[0],
+        g = rgb[1],
+        b = rgb[2],
+        min = Math.min(r, g, b),
+        max = Math.max(r, g, b),
+        delta = max - min,
+        h, s, v;
+
+    if (max == 0)
+      s = 0;
+    else
+      s = (delta/max * 1000)/10;
+
+    if (max == min)
+      h = 0;
+    else if (r == max)
+      h = (g - b) / delta;
+    else if (g == max)
+      h = 2 + (b - r) / delta;
+    else if (b == max)
+      h = 4 + (r - g) / delta;
+
+    h = Math.min(h * 60, 360);
+
+    if (h < 0)
+      h += 360;
+
+    v = ((max / 255) * 1000) / 10;
+
+    return [h, s, v];
+  },
+
+  hwb: function(val) {
+    var r = val[0],
+        g = val[1],
+        b = val[2],
+        h = rgb.hsl(val)[0]
+        w = 1/255 * Math.min(r, Math.min(g, b))
+        b = 1 - 1/255 * Math.max(r, Math.max(g, b));
+
+    return [h, w * 100, b * 100];
+  },
+
+  cmyk: function(rgb) {
+    var r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255,
+        c, m, y, k;
+
+    k = Math.min(1 - r, 1 - g, 1 - b);
+    c = (1 - r - k) / (1 - k);
+    m = (1 - g - k) / (1 - k);
+    y = (1 - b - k) / (1 - k);
+    return [c * 100, m * 100, y * 100, k * 100];
+  },
+
+  xyz: function(rgb) {
+    var r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255;
+
+    // assume sRGB
+    r = r > 0.04045 ? Math.pow(((r + 0.055) / 1.055), 2.4) : (r / 12.92);
+    g = g > 0.04045 ? Math.pow(((g + 0.055) / 1.055), 2.4) : (g / 12.92);
+    b = b > 0.04045 ? Math.pow(((b + 0.055) / 1.055), 2.4) : (b / 12.92);
+
+    var x = (r * 0.4124) + (g * 0.3576) + (b * 0.1805);
+    var y = (r * 0.2126) + (g * 0.7152) + (b * 0.0722);
+    var z = (r * 0.0193) + (g * 0.1192) + (b * 0.9505);
+
+    return [x * 100, y *100, z * 100];
+  },
+
+  lab: function(args) {
+    var xyz = rgb.xyz(args),
+          x = xyz[0],
+          y = xyz[1],
+          z = xyz[2],
+          l, a, b;
+
+    x /= 95.047;
+    y /= 100;
+    z /= 108.883;
+
+    x = x > 0.008856 ? Math.pow(x, 1/3) : (7.787 * x) + (16 / 116);
+    y = y > 0.008856 ? Math.pow(y, 1/3) : (7.787 * y) + (16 / 116);
+    z = z > 0.008856 ? Math.pow(z, 1/3) : (7.787 * z) + (16 / 116);
+
+    l = (116 * y) - 16;
+    a = 500 * (x - y);
+    b = 200 * (y - z);
+
+    return [l, a, b];
+  },
+
+  lch: function(args) {
+    return lab.lch(rgb.lab(args));
+  },
+
+  min: [0,0,0],
+  max: [255,255,255],
+  channel: ['red', 'green', 'blue']
+};
+
+
+var hsl = {
+  rgb: function(hsl) {
+    var h = hsl[0] / 360,
+        s = hsl[1] / 100,
+        l = hsl[2] / 100,
+        t1, t2, t3, rgb, val;
+
+    if (s == 0) {
+      val = l * 255;
+      return [val, val, val];
     }
-  })(func);
 
-  var pair = /(\w+)2(\w+)/.exec(func),
-      from = pair[1],
-      to = pair[2];
+    if (l < 0.5)
+      t2 = l * (1 + s);
+    else
+      t2 = l + s - l * s;
+    t1 = 2 * l - t2;
 
-  // export rgb2hsl and ["rgb"]["hsl"]
-  convert[from] = convert[from] || {};
+    rgb = [0, 0, 0];
+    for (var i = 0; i < 3; i++) {
+      t3 = h + 1 / 3 * - (i - 1);
+      t3 < 0 && t3++;
+      t3 > 1 && t3--;
 
-  convert[from][to] = convert[func] = (function(func) { 
-    return function(arg) {
-      if (typeof arg == "number")
-        arg = Array.prototype.slice.call(arguments);
-      
-      var val = conversions[func](arg);
-      if (typeof val == "string" || val === undefined)
-        return val; // keyword
+      if (6 * t3 < 1)
+        val = t1 + (t2 - t1) * 6 * t3;
+      else if (2 * t3 < 1)
+        val = t2;
+      else if (3 * t3 < 2)
+        val = t1 + (t2 - t1) * (2 / 3 - t3) * 6;
+      else
+        val = t1;
 
-      for (var i = 0; i < val.length; i++)
-        val[i] = Math.round(val[i]);
-      return val;
+      rgb[i] = val * 255;
     }
-  })(func);
-}
 
+    return rgb;
+  },
 
-/* Converter does lazy conversion and caching */
-var Converter = function() {
-   this.convs = {};
+  hsv: function(hsl) {
+    var h = hsl[0],
+        s = hsl[1] / 100,
+        l = hsl[2] / 100,
+        sv, v;
+    l *= 2;
+    s *= (l <= 1) ? l : 2 - l;
+    v = (l + s) / 2;
+    sv = (2 * s) / (l + s);
+    return [h, sv * 100, v * 100];
+  },
+
+  hwb: function(args) {
+    return rgb.hwb(hsl.rgb(args));
+  },
+
+  cmyk: function(args) {
+    return rgb.cmyk(hsl.rgb(args));
+  },
+
+  min: [0,0,0],
+  max: [360,100,100],
+  channel: ['hue', 'saturation', 'lightness']
 };
 
-/* Either get the values for a space or
-  set the values for a space, depending on args */
-Converter.prototype.routeSpace = function(space, args) {
-   var values = args[0];
-   if (values === undefined) {
-      // color.rgb()
-      return this.getValues(space);
-   }
-   // color.rgb(10, 10, 10)
-   if (typeof values == "number") {
-      values = Array.prototype.slice.call(args);        
-   }
 
-   return this.setValues(space, values);
+var hsv = {
+  rgb: function(hsv) {
+    var h = hsv[0] / 60,
+        s = hsv[1] / 100,
+        v = hsv[2] / 100,
+        hi = Math.floor(h) % 6;
+
+    var f = h - Math.floor(h),
+        p = 255 * v * (1 - s),
+        q = 255 * v * (1 - (s * f)),
+        t = 255 * v * (1 - (s * (1 - f))),
+        v = 255 * v;
+
+    switch(hi) {
+      case 0:
+        return [v, t, p];
+      case 1:
+        return [q, v, p];
+      case 2:
+        return [p, v, t];
+      case 3:
+        return [p, q, v];
+      case 4:
+        return [t, p, v];
+      case 5:
+        return [v, p, q];
+    }
+  },
+
+  hsl: function(hsv) {
+    var h = hsv[0],
+        s = hsv[1] / 100,
+        v = hsv[2] / 100,
+        sl, l;
+
+    l = (2 - s) * v;
+    sl = s * v;
+    sl /= (l <= 1) ? l : 2 - l;
+    l /= 2;
+    return [h, sl * 100, l * 100];
+  },
+
+  hwb: function(args) {
+    return rgb.hwb(hsv.rgb(args))
+  },
+
+  cmyk: function(args) {
+    return rgb.cmyk(hsv.rgb(args));
+  },
+
+  min: [0,0,0],
+  max: [360,0,0],
+  channel: ['hue', 'saturation', 'value'],
+  alias: ['hsb']
 };
-  
-/* Set the values for a space, invalidating cache */
-Converter.prototype.setValues = function(space, values) {
-   this.space = space;
-   this.convs = {};
-   this.convs[space] = values;
-   return this;
+
+
+var hwb = {
+  // http://dev.w3.org/csswg/css-color/#hwb-to-rgb
+  rgb: function(hwb) {
+    var h = hwb[0] / 360,
+        wh = hwb[1] / 100,
+        bl = hwb[2] / 100,
+        ratio = wh + bl,
+        i, v, f, n;
+
+    // wh + bl cant be > 1
+    if (ratio > 1) {
+      wh /= ratio;
+      bl /= ratio;
+    }
+
+    i = Math.floor(6 * h);
+    v = 1 - bl;
+    f = 6 * h - i;
+    if ((i & 0x01) != 0) {
+      f = 1 - f;
+    }
+    n = wh + f * (v - wh);  // linear interpolation
+
+    switch (i) {
+      default:
+      case 6:
+      case 0: r = v; g = n; b = wh; break;
+      case 1: r = n; g = v; b = wh; break;
+      case 2: r = wh; g = v; b = n; break;
+      case 3: r = wh; g = n; b = v; break;
+      case 4: r = n; g = wh; b = v; break;
+      case 5: r = v; g = wh; b = n; break;
+    }
+
+    return [r * 255, g * 255, b * 255];
+  },
+
+  hsl: function(args) {
+    return rgb.hsl(hwb.rgb(args));
+  },
+
+  hsv: function(args) {
+    return rgb.hsv(hwb.rgb(args));
+  },
+
+  cmyk: function(args) {
+    return rgb.cmyk(hwb.rgb(args));
+  },
+
+  min: [0,0,0],
+  max: [360,100,100],
+  channel: ['hue', 'whiteness', 'blackness']
 };
 
-/* Get the values for a space. If there's already
-  a conversion for the space, fetch it, otherwise
-  compute it */
-Converter.prototype.getValues = function(space) {
-   var vals = this.convs[space];
-   if (!vals) {
-      var fspace = this.space,
-          from = this.convs[fspace];
-      vals = convert[fspace][space](from);
 
-      this.convs[space] = vals;
-   }
-  return vals;
+var cmyk = {
+  rgb: function(cmyk) {
+    var c = cmyk[0] / 100,
+        m = cmyk[1] / 100,
+        y = cmyk[2] / 100,
+        k = cmyk[3] / 100,
+        r, g, b;
+
+    r = 1 - Math.min(1, c * (1 - k) + k);
+    g = 1 - Math.min(1, m * (1 - k) + k);
+    b = 1 - Math.min(1, y * (1 - k) + k);
+    return [r * 255, g * 255, b * 255];
+  },
+
+  hsl: function(args) {
+    return rgb.hsl(cmyk.rgb(args));
+  },
+
+  hsv: function(args) {
+    return rgb.hsv(cmyk.rgb(args));
+  },
+
+  hwb: function(args) {
+    return rgb.hwb(cmyk.rgb(args));
+  },
+
+  min: [0,0,0,0],
+  max: [100,100,100,100],
+  channel: ['cyan', 'magenta', 'yellow', 'black']
 };
 
-["rgb", "hsl", "hsv", "cmyk", "keyword"].forEach(function(space) {
-   Converter.prototype[space] = function(vals) {
-      return this.routeSpace(space, arguments);
-   }
-});
 
-module.exports = convert;
+//TODO: add illuminant info?
+var xyz = {
+  rgb: function(xyz) {
+    var x = xyz[0] / 100,
+        y = xyz[1] / 100,
+        z = xyz[2] / 100,
+        r, g, b;
+
+    r = (x * 3.2406) + (y * -1.5372) + (z * -0.4986);
+    g = (x * -0.9689) + (y * 1.8758) + (z * 0.0415);
+    b = (x * 0.0557) + (y * -0.2040) + (z * 1.0570);
+
+    // assume sRGB
+    r = r > 0.0031308 ? ((1.055 * Math.pow(r, 1.0 / 2.4)) - 0.055)
+      : r = (r * 12.92);
+
+    g = g > 0.0031308 ? ((1.055 * Math.pow(g, 1.0 / 2.4)) - 0.055)
+      : g = (g * 12.92);
+
+    b = b > 0.0031308 ? ((1.055 * Math.pow(b, 1.0 / 2.4)) - 0.055)
+      : b = (b * 12.92);
+
+    r = Math.min(Math.max(0, r), 1);
+    g = Math.min(Math.max(0, g), 1);
+    b = Math.min(Math.max(0, b), 1);
+
+    return [r * 255, g * 255, b * 255];
+  },
+
+  lab: function(xyz) {
+    var x = xyz[0],
+        y = xyz[1],
+        z = xyz[2],
+        l, a, b;
+
+    x /= 95.047;
+    y /= 100;
+    z /= 108.883;
+
+    x = x > 0.008856 ? Math.pow(x, 1/3) : (7.787 * x) + (16 / 116);
+    y = y > 0.008856 ? Math.pow(y, 1/3) : (7.787 * y) + (16 / 116);
+    z = z > 0.008856 ? Math.pow(z, 1/3) : (7.787 * z) + (16 / 116);
+
+    l = (116 * y) - 16;
+    a = 500 * (x - y);
+    b = 200 * (y - z);
+
+    return [l, a, b];
+  },
+
+  lch: function(args) {
+    return lab.lch(xyz.lab(args));
+  },
+
+  min: [0,0,0],
+  max: [100,100,100],
+  channel: ['x','y','z'],
+  alias: ['ciexyz']
+};
+
+
+var lab = {
+  xyz: function(lab) {
+    var l = lab[0],
+        a = lab[1],
+        b = lab[2],
+        x, y, z, y2;
+
+    if (l <= 8) {
+      y = (l * 100) / 903.3;
+      y2 = (7.787 * (y / 100)) + (16 / 116);
+    } else {
+      y = 100 * Math.pow((l + 16) / 116, 3);
+      y2 = Math.pow(y / 100, 1/3);
+    }
+
+    x = x / 95.047 <= 0.008856 ? x = (95.047 * ((a / 500) + y2 - (16 / 116))) / 7.787 : 95.047 * Math.pow((a / 500) + y2, 3);
+
+    z = z / 108.883 <= 0.008859 ? z = (108.883 * (y2 - (b / 200) - (16 / 116))) / 7.787 : 108.883 * Math.pow(y2 - (b / 200), 3);
+
+    return [x, y, z];
+  },
+
+  lch: function(lab) {
+    var l = lab[0],
+        a = lab[1],
+        b = lab[2],
+        hr, h, c;
+
+    hr = Math.atan2(b, a);
+    h = hr * 360 / 2 / Math.PI;
+    if (h < 0) {
+      h += 360;
+    }
+    c = Math.sqrt(a * a + b * b);
+    return [l, c, h];
+  },
+
+  rgb: function(args) {
+    return xyz.rgb(lab.xyz(args));
+  },
+
+  min: [0,-100,-100],
+  max: [100,100,100],
+  channel: ['lightness', 'a', 'b'],
+  alias: ['cielab']
+};
+
+
+var lch = {
+  lab: function(lch) {
+    var l = lch[0],
+        c = lch[1],
+        h = lch[2],
+        a, b, hr;
+
+    hr = h / 360 * 2 * Math.PI;
+    a = c * Math.cos(hr);
+    b = c * Math.sin(hr);
+    return [l, a, b];
+  },
+
+  xyz: function(args) {
+    return lab.xyz(lch.lab(args));
+  },
+
+  rgb: function(args) {
+    return lab.rgb(lch.lab(args));
+  },
+
+  min: [0,0,0],
+  max: [100,100,360],
+  channel: ['lightness', 'chroma', 'hue'],
+  alias: ['cielch']
+};
+
+
+
+/**
+ * @module color-space
+ */
+module.exports = {
+  rgb: rgb,
+  hsl: hsl,
+  hsv: hsv,
+  hwb: hwb,
+  cmyk: cmyk,
+  xyz: xyz,
+  lab: lab,
+  lch: lch
+};
