248240574f672f5e0729149c25cebb849cce92c9
Dmitry <df.creative@gmail.com>
2016-02-01 13:00:13 -0500

    Uncomment tests

--- test/index.js
+++ test/index.js
@@ -756,7 +756,8 @@ describe('osaucs', function () {
 		// assert.deepEqual(round(s.osaucs.xyy([-8,-6,+2])), [1.773902, 1.049996, 7.893570]);
 		// assert.deepEqual(round(s.osaucs.xyy(s.xyy.osaucs([10,20,30]))), [10,20,30]);
 	});
-	it.only('xyy → osaucs', function () {
+	it('xyy → osaucs', function () {
+		//TODO: fix test according to the paper
 		// assert.deepEqual(round(s.xyz.osaucs([33.71, 26.46, 46.66])), [0,-4,-4]);
 		assert.deepEqual(round(s.xyz.osaucs([33.71, 26.46, 46.66])), [0,-4,-5]);
 		// assert.deepEqual(round(s.xyz.osaucs([1.773902, 1.049996, 7.893570])), [-8,-6,+2]);
