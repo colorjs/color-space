822a488ad752326f85bd5491a853a4a5c32adc59
dy <df.creative@gmail.com>
2022-01-24 22:36:25 -0500

    Simplify hsl formula

--- hsl.js
+++ hsl.js
@@ -11,48 +11,22 @@ export default {
 	alias: ['HSL'],
 
 	rgb: function(hsl) {
-		var h = hsl[0] / 360,
-				s = hsl[1] / 100,
-				l = hsl[2] / 100,
-				t1, t2, t3, rgb, val;
+		var h = hsl[0]/360, s = hsl[1]/100, l = hsl[2]/100, t1, t2, t3, rgb, val, i=0;
 
-		if (s === 0) {
-			val = l * 255;
-			return [val, val, val];
-		}
+		if (s === 0) return val = l * 255, [val, val, val];
 
-		if (l < 0.5) {
-			t2 = l * (1 + s);
-		}
-		else {
-			t2 = l + s - l * s;
-		}
+		t2 = l < 0.5 ? l * (1 + s) : l + s - l * s;
 		t1 = 2 * l - t2;
 
 		rgb = [0, 0, 0];
-		for (var i = 0; i < 3; i++) {
+		for (;i<3;) {
 			t3 = h + 1 / 3 * - (i - 1);
-			if (t3 < 0) {
-				t3++;
-			}
-			else if (t3 > 1) {
-				t3--;
-			}
-
-			if (6 * t3 < 1) {
-				val = t1 + (t2 - t1) * 6 * t3;
-			}
-			else if (2 * t3 < 1) {
-				val = t2;
-			}
-			else if (3 * t3 < 2) {
-				val = t1 + (t2 - t1) * (2 / 3 - t3) * 6;
-			}
-			else {
-				val = t1;
-			}
-
-			rgb[i] = val * 255;
+			t3 < 0 ? t3++ : t3 > 1 && t3--;
+			val = 6 * t3 < 1 ? t1 + (t2 - t1) * 6 * t3 :
+			2 * t3 < 1 ? t2 :
+			3 * t3 < 2 ?  t1 + (t2 - t1) * (2 / 3 - t3) * 6 :
+			t1;
+			rgb[i++] = val * 255;
 		}
 
 		return rgb;
