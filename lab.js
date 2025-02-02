396d82bdfcca8f619079580286de439cff16d46a
Dmitry Iv. <df.creative@gmail.com>
2025-02-01 22:15:22 -0500

    Add oklab rgb

--- lab.js
+++ lab.js
@@ -12,42 +12,32 @@ var lab = {
 	max: [100, 100, 100],
 	channel: ['lightness', 'a', 'b'],
 	alias: ['LAB', 'cielab'],
+}
 
-	xyz: function (lab) {
-		var l = lab[0],
-			a = lab[1],
-			b = lab[2],
-			x, y, z, y2;
+lab.xyz = ([l, a, b]) => {
+	var x, y, z, y2;
 
-		if (l <= 8) {
-			y = (l * 100) / 903.3;
-			y2 = (7.787 * (y / 100)) + (16 / 116);
-		} else {
-			y = 100 * Math.pow((l + 16) / 116, 3);
-			y2 = Math.pow(y / 100, 1 / 3);
-		}
-
-		//FIXME x is undefined!
-		//@ts-ignore
-		x = x / 95.047 <= 0.008856 ? x = (95.047 * ((a / 500) + y2 - (16 / 116))) / 7.787 : 95.047 * Math.pow((a / 500) + y2, 3);
-
-		//FIXME z is undefined!
-		//@ts-ignore
-		z = z / 108.883 <= 0.008859 ? z = (108.883 * (y2 - (b / 200) - (16 / 116))) / 7.787 : 108.883 * Math.pow(y2 - (b / 200), 3);
-
-		return [x, y, z];
+	if (l <= 8) {
+		y = (l * 100) / 903.3;
+		y2 = (7.787 * (y / 100)) + (16 / 116);
+	} else {
+		y = 100 * Math.pow((l + 16) / 116, 3);
+		y2 = Math.pow(y / 100, 1 / 3);
 	}
-};
+
+	x = x / 95.047 <= 0.008856 ? x = (95.047 * ((a / 500) + y2 - (16 / 116))) / 7.787 : 95.047 * Math.pow((a / 500) + y2, 3);
+
+	z = z / 108.883 <= 0.008859 ? z = (108.883 * (y2 - (b / 200) - (16 / 116))) / 7.787 : 108.883 * Math.pow(y2 - (b / 200), 3);
+
+	return [x, y, z];
+}
+
 
 export default (lab);
 
 
-//extend xyz
-xyz.lab = function (xyz) {
-	var x = xyz[0],
-		y = xyz[1],
-		z = xyz[2],
-		l, a, b;
+xyz.lab = ([x, y, z]) => {
+	var l, a, b;
 
 	x /= 95.047;
 	y /= 100;
